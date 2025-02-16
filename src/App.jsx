d5a66b929caf90e831a58c89cec04119dc6fbeea
rlahteen <lahteri@gmail.com>
2025-02-16 20:29:45 +0200

    KAN-1 <buttons added>

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,59 @@ import { useState, useEffect } from 'react'
 import Feed from './Feed';
 function App() {
   const [articles, setArticles] = useState([]);
+  const [source, setSource] = useState('https://rssvercel.vercel.app/yle');
+  
+  const sources = [
+    { value: 'https://rssvercel.vercel.app/yle', label: 'YLE' },
+    { value: 'https://rssvercel.vercel.app/hs', label: 'Helsingin Sanomat' },
+    { value: 'https://rssvercel.vercel.app/euro', label: 'European Newsroom' },
+  ]; 
+  /* const sources = [
+    { value: 'http://localhost:4000/euro', label: 'European Newsroom' },
+    { value: 'http://localhost:4000/yle', label: 'YLE' },
+    { value: 'http://localhost:4000/hs', label: 'Helsingin Sanomat' },
+  ]; */
 
   console.log(articles);
   const fetchArticles = async () => {
     try {
-      const response = await axios.get('https://rssvercel.vercel.app/articles');
+      const response = await axios.get(source);
       setArticles(response.data);
     } catch (error) {
       console.error('Error fetching articles:', error);
     }
   };
 
+
+  // Päivittää lähteen ja hakee artikkelit
+  const handleSourceChange = (event) => {
+    const newSource = event.target.value;
+    setSource(newSource);
+    //fetchArticles(source); // Varmistaa, että artikkelit päivittyvät heti
+  };
+
+  // Hakee artikkelit alussa
   useEffect(() => {
-    fetchArticles();
-  }, []);
+    fetchArticles(source);
+  }, [source]); // Tyhjä dependency array -> ajetaan vain kerran
   
   return (
     <>
-      <h1>RSS Feed</h1>
+      <h1>News</h1>
+      <h3>Select source</h3>
+      {sources.map((src) => (
+        <label key={src.value} style={{ marginRight: '10px' }}>
+          <input
+            type="radio"
+            name="rss_source"
+            value={src.value}
+            checked={source === src.value}
+            onChange={handleSourceChange}
+          />
+          {src.label}
+        </label>
+      ))}
+      
       {articles.map((item, index) => (
             <Feed
                 key={index}
--- src/Feed.jsx
+++ src/Feed.jsx
@@ -3,7 +3,7 @@
  * Created Date: Tuesday February 4th 2025 01:38:56
  * Author: ristoxxx@github.com
  * -----
- * Last Modified: Sunday February 9th 2025 05:50:32
+ * Last Modified: Sunday February 16th 2025 08:05:35
  * Modified By: ristoxxx@github.com
  * -----
  * Copyright (c) 2025 ristoxxx@github.com
@@ -14,7 +14,7 @@ const Feed = ({ title, link }) => {
   return (
     <div>
       <h2>{title}</h2><br />
-      <a href={link} class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Lue lisää</a>
+      <a href={link} className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Lue lisää</a>
       <br></br>
       <br></br>
     </div>
